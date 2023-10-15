1c68d62d88d302ad4681ec0586c60bbfb9b5aab9
VinsonAlter <vinson7768@gmail.com>
2023-10-15 22:13:40 +0700

    finish idea until lesson 21

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,7 +16,7 @@ class AuthController extends Controller
         $validated = request()->validate([
             'name' => 'required|min:3|max:40',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|confirmed|min:8'
+            'password' => 'required|confirmed|min:5'
         ]);
 
         User::create([
@@ -27,4 +27,41 @@ class AuthController extends Controller
 
         return redirect()->route('dashboard')->with('success', 'User registered successfully!');
     }
+
+    public function login() {
+        return view('auth.login');
+    }
+
+    public function authenticate() {
+        // dd(request()->all());
+        
+        $validated = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:5'
+        ]);
+
+        // User::create([
+        //     'name' => $validated['name'],
+        //     'email' => $validated['email'],
+        //     'password' => Hash::make($validated['password'])
+        // ]);
+
+        if(auth()->attempt($validated)) {
+            // remember session regenerate means clear session after this auth done successfully
+            request()->session()->regenerate();
+            return redirect()->route('dashboard')->with('success', 'User logged in successfully');
+        }
+        
+        return redirect()->route('login')->withErrors([
+            'email' => 'No matching user found with the email and password'
+        ]);
+    }
+
+    public function logout() {
+        auth()->logout();
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+        return redirect()->route('dashboard')->with('success', 'logged out successfully');
+    }
+
 }
