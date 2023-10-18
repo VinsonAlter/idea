94c30609c30d9c10ea9c0fd423da9b076d8a56ab
VinsonAlter <vinson7768@gmail.com>
2023-10-18 22:22:01 +0700

    update until lesson 24

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Idea;
 use App\Models\Comment;
+use Illuminate\Support\Facades\Auth;
 
 class CommentController extends Controller
 {
@@ -12,7 +13,7 @@ class CommentController extends Controller
         // dd($request->content);
         $comment = new Comment;
         $comment->idea_id = $idea->id;
-        $comment->user_id = auth()->id;
+        $comment->user_id = Auth::user()->id;
         $comment->content = $request->content;
         $comment->save();
         return redirect()->route('ideas.show', $idea->id)->with('success', 'Comment posted successfully' );
--- routes/web.php
+++ routes/web.php
@@ -19,29 +19,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::group(['prefix' => 'ideas/', 'as' => 'ideas.'], function() {
-    // via withoutMiddleware Method
-    // Route::post('', [IdeaController::class, 'store'])->name('store')->withoutMiddleware(['auth']);
-    // Route::get('{idea}', [IdeaController::class, 'show'])->name('show')->withoutMiddleware(['auth']);
-    Route::post('', [IdeaController::class, 'store'])->name('store');
-    Route::get('{idea}', [IdeaController::class, 'show'])->name('show');
+//     // via withoutMiddleware Method
+//     // Route::post('', [IdeaController::class, 'store'])->name('store')->withoutMiddleware(['auth']);
+//     // Route::get('{idea}', [IdeaController::class, 'show'])->name('show')->withoutMiddleware(['auth']);
+    // Route::get('{idea}', [IdeaController::class, 'show'])->name('show');
 
     Route::group(['middleware' => 'auth'], function() {
-        Route::get('{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
-        Route::put('{idea}', [IdeaController::class, 'update'])->name('update');
-        Route::delete('{idea}', [IdeaController::class, 'destroy'])->name('destroy');
-        Route::post('{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
+         // Route::post('', [IdeaController::class, 'store'])->name('store');
+//         Route::get('{idea}/edit', [IdeaController::class, 'edit'])->name('edit');
+//         Route::put('{idea}', [IdeaController::class, 'update'])->name('update');
+//         Route::delete('{idea}', [IdeaController::class, 'destroy'])->name('destroy');
+        // Route::post('{idea}/comments', [CommentController::class, 'store'])->name('comments.store');
     });
 });
 
-Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::resource('ideas', IdeaController::class)->except(['index', 'create', 'show'])->middleware('auth');
 
-Route::post('/register', [AuthController::class, 'store']);
+// adding route resource but without middleware for class show
+Route::resource('ideas', IdeaController::class)->only(['show']);
 
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-
-Route::post('/login', [AuthController::class, 'authenticate']);
-
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+// ideas/{idea}/comments
+// to pass these to laravel route:resource, you can
+Route::resource('ideas.comments', CommentController::class)->only(['store'])->middleware('auth');
 
 Route::get('/terms', function() {
     return view('terms');
