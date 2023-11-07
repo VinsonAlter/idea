efbc889f9ff7e64e4d72547efa2a4c707bfa4136
VinsonAlter <vinson7768@gmail.com>
2023-11-07 22:30:12 +0700

    almost finish laravel tutorial

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UpdateUserRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
@@ -39,17 +40,27 @@ class UserController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * 
      */
-    public function update(User $user)
+    public function update(UpdateUserRequest $request, User $user)
     {
-        $this->authorize('update', $user);
-        $validated = request()->validate([
-            'name' => 'required|min:3|max:40',
-            'bio' => 'nullable|min:1|max:255',
-            'image' => 'image',
-        ]);
+        $this->authorize('update', $user);  
+        // $validated = request()->validate([
+        //     'name' => 'required|min:3|max:40',
+        //     'bio' => 'nullable|min:1|max:255',
+        //     'image' => 'image',
+        // ]);
 
-        if(request()->has('image')) {
-            $imagePath = request()->file('image')->store('profile', 'public');
+        // set the validation via Form Request
+        $validated = $request->validated();
+
+        // if(request()->has('image')) {
+        //     $imagePath = request()->file('image')->store('profile', 'public');
+        //     $validated['image'] = $imagePath;
+
+        //     Storage::disk('public')->delete($user->image ?? '');
+        // }
+
+        if($request->has('image')) {
+            $imagePath = $request->file('image')->store('profile', 'public');
             $validated['image'] = $imagePath;
 
             Storage::disk('public')->delete($user->image ?? '');
---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        // return $this->user()->can('update', $this->user);
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'required|min:3|max:40',
+            'bio' => 'nullable|min:1|max:255',
+            'image' => 'image',
+        ];
+    }
+}
